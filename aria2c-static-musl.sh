2eadb8d38df513141e5b6936d51d012c85a83633
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-07 06:37:36 -0500

    patch

--- aria2c-static-musl.sh
+++ aria2c-static-musl.sh
@@ -12,35 +12,22 @@ TOMATO="\033[38;2;255;99;71m"
 NC="\033[0m"
 
 ARCH=${ARCH:-x86_64}
+ALPINE_VERSION="3.23.3"
+ALPINE_MAJOR_MINOR="${ALPINE_VERSION%.*}"
 
-## map arch to Alpine minirootfs URL and QEMU binary name
 case "${ARCH}" in
-  x86_64)
-    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-minirootfs-3.23.3-x86_64.tar.gz"
-    QEMU_ARCH=""
-    ;;
-  x86)
-    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86/alpine-minirootfs-3.23.3-x86.tar.gz"
-    QEMU_ARCH="i386"
-    ;;
-  aarch64)
-    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.3-aarch64.tar.gz"
-    QEMU_ARCH="aarch64"
-    ;;
-  armhf)
-    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/armhf/alpine-minirootfs-3.23.3-armhf.tar.gz"
-    QEMU_ARCH="arm"
-    ;;
-  armv7)
-    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/armv7/alpine-minirootfs-3.23.3-armv7.tar.gz"
-    QEMU_ARCH="arm"
-    ;;
+  x86_64)  QEMU_ARCH="" ;;
+  x86)     QEMU_ARCH="i386" ;;
+  aarch64) QEMU_ARCH="aarch64" ;;
+  armhf)   QEMU_ARCH="arm" ;;
+  armv7)   QEMU_ARCH="arm" ;;
   *)
     echo "Unknown architecture: ${ARCH}"
     exit 1
     ;;
 esac
 
+ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_MAJOR_MINOR}/releases/${ARCH}/alpine-minirootfs-${ALPINE_VERSION}-${ARCH}.tar.gz"
 TARBALL="${ALPINE_URL##*/}"
 
 ## unmount bind mounts on exit to avoid leaking mounts on failure
@@ -52,11 +39,9 @@ cleanup() {
 trap cleanup EXIT
 
 echo -e "${AQUA}= install dependencies${NC}"
-PKGS=(wget curl binutils)
-if [ -n "${QEMU_ARCH}" ]; then
-  PKGS+=(qemu-user-static)
-fi
-sudo apt-get update -qy && sudo apt-get install -y "${PKGS[@]}"
+DEBIAN_DEPS=(wget curl binutils)
+[ -n "${QEMU_ARCH}" ] && DEBIAN_DEPS+=(qemu-user-static)
+sudo apt-get update -qy && sudo apt-get install -y "${DEBIAN_DEPS[@]}"
 
 echo -e "${AQUA}= fetching latest aria2 version${NC}"
 ARIA2_VERSION=$(curl -fsSL "https://api.github.com/repos/aria2/aria2/releases/latest" \
@@ -79,7 +64,7 @@ cp /etc/resolv.conf ./pasta/etc/
 
 if [ -n "${QEMU_ARCH}" ]; then
   echo -e "${TAWNY}= setup QEMU for cross-arch builds${NC}"
-  sudo mkdir -p ./pasta/usr/bin/
+  sudo mkdir -p "./pasta/usr/bin/"
   sudo cp "/usr/bin/qemu-${QEMU_ARCH}-static" "./pasta/usr/bin/"
 fi
 
@@ -102,6 +87,7 @@ libpsl-static \
 flex \
 bison \
 upx \
+patch \
 autoconf \
 automake \
 libtool \
@@ -120,13 +106,16 @@ lz4-static \
 libgpg-error-dev \
 libgpg-error-static \
 perl && curl -fsSL -O 'https://github.com/aria2/aria2/releases/download/release-${ARIA2_VERSION}/aria2-${ARIA2_VERSION}.tar.gz' && \
+curl -fsSL -O 'https://github.com/gfunkmonk/aria2c-static-musl/raw/refs/heads/main/aria2-1.37.0.conf.patch' && \
 tar xf aria2-${ARIA2_VERSION}.tar.gz && \
 cd aria2-${ARIA2_VERSION}/ && \
+patch -p1 < ../aria2-1.37.0.conf.patch && \
 ./configure CC=gcc ARIA2_STATIC=yes --without-gnutls --with-openssl --disable-bittorrent LDFLAGS='-static' CFLAGS='-O3 -Wno-unterminated-string-initialization' && \
 make -j\$(nproc) && \
 strip src/aria2c && \
 upx --ultra-brute src/aria2c"
 mkdir -p dist
 cp "./pasta/aria2-${ARIA2_VERSION}/src/aria2c" "dist/aria2c-${ARCH}"
+if command -v file >/dev/null 2>&1; then echo -e "${ORANGE} File Info:  $(file "dist/aria2c-${ARCH}" | cut -d: -f2-)${NC}"; fi
 tar -C dist -cJf "dist/aria2c-${ARCH}.tar.xz" "aria2c-${ARCH}"
 echo -e "${LEMON}= All done! Binary: dist/aria2c-${ARCH} ($(du -sh "dist/aria2c-${ARCH}" | cut -f1))${NC}"
